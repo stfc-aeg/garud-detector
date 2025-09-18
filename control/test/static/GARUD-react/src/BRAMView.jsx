4e612ed54284941747de8e00f67b2cafadc2afb3
Joseph Nobes <joseph.nobes@stfc.ac.uk>
2025-09-18 17:18:51 +0100

    Added sliders for frame selection, and different titles for combined view

--- control/test/static/GARUD-react/src/BRAMView.jsx
+++ control/test/static/GARUD-react/src/BRAMView.jsx
@@ -5,6 +5,80 @@ import "./styles.css";
 import Plot from "react-plotly.js";
 import { TitleCard } from "odin-react";
 
+function DisplayFrameStartInput(props) {
+  function Send(event, endpoint) {
+    endpoint
+      .put(
+        { ["display_start_frame"]: Number(event.target.value) },
+        "application/bram/control"
+      )
+      .then((response) => {
+        endpoint.mergeData(response, "application/bram/control");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  return (
+    <>
+      <div style={{ width: "100%" }}>
+        <p style={{ width: "240px", display: "inline-block" }}>
+          Starting frame (up to {props.periodicEndpoint.data.application.bram.control.max_frame}):
+        </p>
+        <input
+          onChange={(event) => Send(event, props.periodicEndpoint)}
+          className="DisplayFrameStartInput"
+          type="range"
+          min="0"
+          max={props.periodicEndpoint.data.application.bram.control.max_frame}
+        />
+        <p
+          style={{ width: "30px", marginLeft: "10px", display: "inline-block" }}
+        >
+          {props.periodicEndpoint.data.application.bram.control.display_start_frame}
+        </p>
+      </div>
+    </>
+  );
+}
+
+function DisplayFrameNumInput(props) {
+  function Send(event, endpoint) {
+    endpoint
+      .put(
+        { ["display_num_frames"]: Number(event.target.value) },
+        "application/bram/control"
+      )
+      .then((response) => {
+        endpoint.mergeData(response, "application/bram/control");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  return (
+    <>
+      <div style={{ width: "100%" }}>
+        <p style={{ width: "240px", display: "inline-block" }}>
+          Number of frames to display:
+        </p>
+        <input
+          onChange={(event) => Send(event, props.periodicEndpoint)}
+          className="DisplayFrameNumInput"
+          type="range"
+          min="1"
+          max={props.periodicEndpoint.data.application.bram.control.max_frame - props.periodicEndpoint.data.application.bram.control.display_start_frame}
+        />
+        <p
+          style={{ width: "30px", marginLeft: "10px", display: "inline-block" }}
+        >
+          {props.periodicEndpoint.data.application.bram.control.display_num_frames}
+        </p>
+      </div>
+    </>
+  );
+}
+
 // a plotly component, to create a heatmap in black and white. The use of memo makes it only rerender when the isEqual function is false
 const Heatmap = React.memo((props) => {
   return (
@@ -63,7 +137,7 @@ function TriggerReadButton(props) {
   function Send() {
     //set the flag in the parameter tree to true to trigger a read
     props.endpoint
-      .put({ [props.key]: true }, "application/bram/control")
+      .put({ ["refresh"]: true }, "application/bram/control")
       .then((response) => {
         props.endpoint.mergeData(response, "application/bram/control");
       })
@@ -106,8 +180,9 @@ function BRAMFrameHeatmap(props) {
 function BRAMFrames(props) {
     var framerows = [];
     for (let frame_number of Object.keys(props.periodicEndpoint.data.application.bram.data)) {
+        var title = props.periodicEndpoint.data.application.bram.control.display_combined ? "BRAM Combined frames  " + props.periodicEndpoint.data.application.bram.control.display_start_frame + " - " + props.periodicEndpoint.data.application.bram.control.display_num_frames : "BRAM Output frame " + frame_number;
         framerows.push(
-          <TitleCard title={"BRAM Output frame " + frame_number}>
+          <TitleCard title={title}>
             {props.periodicEndpoint.data.application.bram.data[frame_number] != null ? (
               <BRAMFrameHeatmap periodicEndpoint={props.periodicEndpoint} frame_number={frame_number} />
             ) : (
@@ -134,7 +209,6 @@ export default function BRAM_View(props) {
                 <div className="mytooltip">
                   <TriggerReadButton
                     endpoint={props.periodicEndpoint}
-                    key={"refresh"}
                     name={"Refresh"}
                   />
                   {
@@ -148,6 +222,23 @@ export default function BRAM_View(props) {
             </div>
             <div style={{ width: "49%", float: "right" }}>
               <TitleCard title="Read settings">
+                <div className="mytooltip">
+                  <DisplayFrameStartInput periodicEndpoint={props.periodicEndpoint} />
+                  {
+                    <span className="mytooltiptext">
+                      {"Function: UPDATEME"}
+                    </span>
+                  }
+                </div>
+                <div style={{ width: "100%", height: "20px" }}></div>
+                <div className="mytooltip">
+                  <DisplayFrameNumInput periodicEndpoint={props.periodicEndpoint} />
+                  {
+                    <span className="mytooltiptext">
+                      {"Function: UPDATEME"}
+                    </span>
+                  }
+                </div>
               </TitleCard>
             </div>
           </TitleCard>
